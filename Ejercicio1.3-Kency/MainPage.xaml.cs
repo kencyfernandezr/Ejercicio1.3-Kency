42784d41e45788ac55ba1cdf95ad850a0ecbc4e8
Kency Fernandez <Kency Fernandez@KENCY>
2023-03-05 20:44:44 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1.3-Kency/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1._3_Kency
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
